248dad3747b778e76298f19ea6bc74f767918409
tunguyen <tunguyenhsgs@gmail.com>
2019-05-02 21:14:37 +0700

    02.05.19

--- prepare_data.py
+++ prepare_data.py
@@ -7,8 +7,13 @@ if __name__ == '__main__':
     dDir = '/media/tunguyen/Others/Dataset/assembly_data/CodeChef_Data_ASM_Seq'
     # list dataset file
     for file in os.listdir(dDir):
+        dataset = file.split('_')[0]
+        op = file.split('_')[2]
+        op = 'NoOp' if op != 'Op' else op
         file_path = os.path.join(dDir, file)
-        folder_path = file_path.split('.txt')[0]
+        # folder_path = file_path.split('.txt')[0]
+        lastname = file.split('.txt')[0].split('_')[-1]
+        folder_path = dDir+'/'+dataset+'/'+op+'/'+dataset+'_Seq_'+op+'_'+lastname
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
